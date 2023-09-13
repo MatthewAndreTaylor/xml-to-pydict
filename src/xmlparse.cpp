fa5c801f16075278fa2e96cdae41809cc369eac2
Matthew Taylor <100451342+MatthewAndreTaylor@users.noreply.github.com>
2023-09-13 01:49:29 -0400

    Added parsing exceptions

--- src/xmlparse.cpp
+++ src/xmlparse.cpp
@@ -18,81 +18,208 @@ typedef struct {
   std::vector<Pair> attr;
 } XMLNode;
 
+size_t i;
+
+static void parseContainerClose(XMLNode* node, const char *xmlContent) {
+  node->type = CONTAINER_CLOSE;
+  i++;
+  if (std::isalpha(xmlContent[i]) || xmlContent[i] == '_' || xmlContent[i] == ':') {
+    node->elementName.push_back(xmlContent[i]);
+    i++;
+  } else {
+    PyErr_Format(PyExc_Exception, "not well formed (naming violation at pos=%d)", i);
+    return;
+  }
+
+  while (xmlContent[i] != '\0' && xmlContent[i] != '>') {
+    if (std::isalnum(xmlContent[i]) || xmlContent[i] == '_' || xmlContent[i] == ':' || xmlContent[i] == '-' || xmlContent[i] == '.') {
+      node->elementName.push_back(xmlContent[i]);
+    } else if (std::isspace(xmlContent[i])){
+      if (node->elementName.empty()) {
+        PyErr_Format(PyExc_Exception, "not well formed (expected Name at pos=%d)", i);
+        return;
+      }
+    } else {
+      PyErr_Format(PyExc_Exception, "not well formed (naming violation at pos=%d)", i);
+      return;
+    }
+    i++;
+  }
+  i++;
+}
+
+static void parseContainerOpen(XMLNode* node, const char *xmlContent) {
+  node->type = CONTAINER_OPEN;
+
+  if (std::isalpha(xmlContent[i]) || xmlContent[i] == '_' || xmlContent[i] == ':') {
+    node->elementName.push_back(xmlContent[i]);
+    i++;
+  } else {
+    PyErr_Format(PyExc_Exception, "not well formed (naming violation at pos=%d)", i);
+    return;
+  }
+
+  bool hasAttr = false;
+
+  char state = 0; // 0: start, 1: name, 2: equals, 3: quote, 4: value
+
+  // Parse name
+  while (xmlContent[i] != '\0' && xmlContent[i] != '>') {
+    if (xmlContent[i] == '/' && xmlContent[i+1] == '>') {
+      node->type = PRIMITIVE;
+      i+=2;
+      return;
+    }
+
+    if (std::isalnum(xmlContent[i]) || xmlContent[i] == '_' || xmlContent[i] == ':' || xmlContent[i] == '-' || xmlContent[i] == '.') {
+      node->elementName.push_back(xmlContent[i]);
+      i++;
+    } else if (std::isspace(xmlContent[i])){
+      if (node->elementName.empty()) {
+        PyErr_Format(PyExc_Exception, "not well formed (no name at pos=%d)", i);
+        return;
+      }
+      hasAttr = true;
+      break;
+    } else {
+      PyErr_Format(PyExc_Exception, "not well formed (naming violation at pos=%d)", i);
+      return;
+    }
+  }
+
+  if (hasAttr) {
+    std::string key;
+    std::string val;
+    char quoteType = 0;
+
+    while (xmlContent[i] != '\0' && xmlContent[i] != '>') {
+      if (state == 0) {
+        if (xmlContent[i] == '/' && xmlContent[i+1] == '>') {
+          node->type = PRIMITIVE;
+          i+=2;
+          return;
+        }
+
+        if (std::isspace(xmlContent[i])) {
+          i++;
+        } else if (std::isalpha(xmlContent[i]) || xmlContent[i] == '_' || xmlContent[i] == ':') {
+          state = 1;
+          key.push_back(xmlContent[i]);
+          i++;
+        } else {
+          PyErr_Format(PyExc_Exception, "not well formed (attribute naming violation at pos=%d)", i);
+          return;
+        }
+      } else if (state == 1) {
+        if (xmlContent[i] == '=') {
+          state = 3;
+        } else if (std::isalnum(xmlContent[i]) || xmlContent[i] == '_' || xmlContent[i] == ':' || xmlContent[i] == '-' || xmlContent[i] == '.') {
+          key.push_back(xmlContent[i]);
+        } else if (std::isspace(xmlContent[i])) {
+          state = 2;
+        } else {
+          PyErr_Format(PyExc_Exception, "not well formed (attribute naming violation at pos=%d)", i);
+          return;
+        }
+        i++;
+      } else if (state == 2) {
+        if (xmlContent[i] == '=') {
+          state = 3;
+        } else if (!std::isspace(xmlContent[i])) {
+          PyErr_Format(PyExc_Exception, "not well formed (attribute naming violation at pos=%d)", i);
+          return;
+        }
+        i++;
+      } else if (state == 3) {
+        if (xmlContent[i] == '\'' || xmlContent[i] == '\"') {
+          state = 4;
+          quoteType = xmlContent[i];
+        } else if (!std::isspace(xmlContent[i])) {
+          PyErr_Format(PyExc_Exception, "not well formed (attribute naming violation at pos=%d)", i);
+          return;
+        }
+        i++;
+      } else {
+        if (xmlContent[i] == quoteType) {
+          state = 0;
+          node->attr.push_back({key, val});
+          key.clear();
+          val.clear();
+        } else {
+          val.push_back(xmlContent[i]);
+        }
+        i++;
+      }
+    }
+  }
+  if (state != 0) {
+    PyErr_Format(PyExc_Exception, "not well formed (attribute naming violation at pos=%d)", i);
+    return;
+  }
+  i++;
+}
+
+static void parseComment(XMLNode* node, const char *xmlContent) {
+  node->type = COMMENT;
+  i++;
+  if (xmlContent[i] != '-' || xmlContent[i+1] != '-') {
+    PyErr_Format(PyExc_Exception, "not well formed (expected '-' at pos=%d)", i);
+  }
+  i+=2;
+
+  while (xmlContent[i] != '\0' || xmlContent[i+1] != '\0') {
+    if (xmlContent[i] == '-' && xmlContent[i + 1] == '-' && xmlContent[i + 2] == '>') {
+      // Found the end of the comment
+      if (xmlContent[i-1] == '-') {
+        PyErr_Format(PyExc_Exception, "not well formed (invalid '-' at pos=%d)", i-1);
+      }
+      i += 3;
+      return;
+    }
+    i++;
+  }
+  PyErr_SetString(PyExc_Exception, "unclosed token");
+}
+
+static void parseText(XMLNode* node, const char *xmlContent) {
+  node->type = TEXT;
+  bool isSpace = false;
+
+  while (xmlContent[i] != '\0' && xmlContent[i] != '<') {
+    if (xmlContent[i] == '&') {
+      PyErr_Format(PyExc_Exception, "not well formed (naming violation at pos=%d)", i);
+    }
+    if (isSpace || !std::isspace(xmlContent[i])) {
+      node->elementName.push_back(xmlContent[i]);
+      isSpace = true;
+    }
+    i++;
+  }
+  while (!node->elementName.empty() && std::isspace(node->elementName.back())) {
+    node->elementName.pop_back();
+  }
+}
+
 static std::vector<XMLNode> splitNodes(const char *xmlContent) {
   std::vector<XMLNode> nodes;
-  std::string key = "";
-  std::string val = "";
+  i = 0;
   
-  size_t i = 0;
   while (xmlContent[i] != '\0') {
     XMLNode node;
     if (xmlContent[i] == '<') {
-      if (xmlContent[i + 1] == '/') {
-        node.type = CONTAINER_CLOSE;
-        while (xmlContent[i] != '>' && xmlContent[i] != '\0') {
-          i++;
-        }
-      } else if (xmlContent[i + 1] == '!') {
-        node.type = COMMENT;
-        while (xmlContent[i] != '>' && xmlContent[i] != '\0') {
-          i++;
-        }
-      } else {
-        node.type = CONTAINER_OPEN;
-        i++;
-      }
-
-      bool hasAttr = false;
-      bool inquotes = false;
-
-      // Extract element
-      while (xmlContent[i] != '\0' && xmlContent[i] != '>') {
-        bool isSpace = std::isspace(xmlContent[i]);
-        if (xmlContent[i] == '/' && xmlContent[i+1] == '>') {
-          node.type = PRIMITIVE;
-        } else if (!hasAttr && isSpace) {
-          hasAttr = true;
-        } else{
-          if (hasAttr) {
-            if (xmlContent[i] == '\'' || xmlContent[i] == '\"') {
-              inquotes = !inquotes;
-              if(!key.empty() && !val.empty() && !inquotes) {
-                node.attr.push_back({key, val});
-                key.clear();
-                val.clear();
-              }
-            } else if (inquotes) {
-              val.push_back(xmlContent[i]);
-            } else if (xmlContent[i] != '=' && !isSpace){
-              key.push_back(xmlContent[i]);
-            }
-          } else {
-            node.elementName.push_back(xmlContent[i]);
-          }
-        }
-        i++;
-      }
       i++;
-
-      nodes.push_back(node);
+      if (xmlContent[i] == '/') {
+        parseContainerClose(&node, xmlContent);
+      } else if (xmlContent[i] == '!') {
+        parseComment(&node, xmlContent);
+      } else {
+        parseContainerOpen(&node, xmlContent);
+      }
     } else {
-      // Text node
-      node.type = TEXT;
-      bool isSpace = false;
-
-      while (xmlContent[i] != '\0' && xmlContent[i] != '<') {
-        if (isSpace || !std::isspace(xmlContent[i])) {
-          node.elementName.push_back(xmlContent[i]);
-          isSpace = true;
-        }
-        i++;
-      }
-      while (!node.elementName.empty() && std::isspace(node.elementName.back())) {
-          node.elementName.pop_back();
-      }
-      if (!node.elementName.empty()) {
-        nodes.push_back(node);
-      }
+      parseText(&node, xmlContent);
+    }
+    if (!node.elementName.empty()) {
+      nodes.push_back(node);
     }
   }
 
@@ -120,8 +247,10 @@ static PyObject *xml_parse(PyObject *self, PyObject *args) {
 
   std::vector<XMLNode> nodes = splitNodes(xmlContent);
   PyObject *currDict = PyDict_New();
+  std::vector<std::string> containerStackNames;
   std::vector<PyObject *> containerStack;
   containerStack.push_back(currDict);
+  containerStackNames.push_back("");
 
   bool isList = false;
 
@@ -153,13 +282,23 @@ static PyObject *xml_parse(PyObject *self, PyObject *args) {
       if (node.type == CONTAINER_OPEN) {
         currDict = d;
         containerStack.push_back(d);
+        containerStackNames.push_back(node.elementName);
       }
     } else if (node.type == CONTAINER_CLOSE) {
+      if (containerStackNames.back() != node.elementName) {
+        PyErr_Format(PyExc_Exception, "tag mismatch (%U does not match the last start tag)", childKey);
+      }
+      containerStackNames.pop_back();
       containerStack.pop_back();
       currDict = containerStack.back();
     }
 
     Py_DECREF(childKey);
+  }
+
+  if (containerStack.size() > 1) {
+    PyErr_Format(PyExc_Exception, "not well formed (%d unclosed tags)", containerStack.size()-1);
+    return NULL;
   }
 
   PyObject *result = containerStack.front();
--- src/xmlparse.py
+++ src/xmlparse.py
@@ -1,98 +1,68 @@
-from enum import Enum
-
-
-class NodeType(Enum):
-    PRIMITIVE = 0
-    CONTAINER_OPEN = 1
-    CONTAINER_CLOSE = 2
-    TEXT = 3
-    COMMENT = 4
-
-
-def splitNodes(xmlContent: str) -> list:
-    nodes: list = []
+def parse(xmlContent: str) -> dict:
     i = 0
     key = "@"
     val = ""
     xmlContent += " "
+    
+    curr_dict = {}
+    containerStack = [curr_dict]
 
     while i < len(xmlContent):
-        node = [None, "", {}]
+        element_name = ""
 
         if xmlContent[i] == '<':
             if xmlContent[i + 1] == '/':
-                node[0] = NodeType.CONTAINER_CLOSE
+                containerStack.pop()
+                curr_dict = containerStack[-1]
                 i = xmlContent.find('>', i + 1)
             elif xmlContent[i + 1] == '!':
-                node[0] = NodeType.COMMENT
                 i = xmlContent.find('>', i + 1)
             else:
-                node[0] = NodeType.CONTAINER_OPEN
                 i += 1
-
-            hasAttr = False
-            inquotes = False
-            while i < len(xmlContent) and xmlContent[i] != '>':
-                isSpace = xmlContent[i].isspace()
-                if xmlContent[i] == '/' and xmlContent[i + 1] == '>':
-                    node[0] = NodeType.PRIMITIVE
-                elif not hasAttr and isSpace:
-                    hasAttr = True
-                else:
-                    if hasAttr:
-                        if xmlContent[i] == '\'' or xmlContent[i] == '\"':
-                            inquotes = not inquotes
-                            if not inquotes and key != "" and val != "":
-                                node[2][key] = val
-                                key = "@"
-                                val = ""
-                        elif inquotes:
-                            val += xmlContent[i]
-                        elif xmlContent[i] != '=' and not isSpace:
-                            key += xmlContent[i]
+                hasAttr = False
+                inquotes = False
+                isContainer = True
+                d = {}
+                while i < len(xmlContent) and xmlContent[i] != '>':
+                    isSpace = xmlContent[i].isspace()
+                    if xmlContent[i] == '/' and xmlContent[i + 1] == '>':
+                        isContainer = False
+                    elif not hasAttr and isSpace:
+                        hasAttr = True
                     else:
-                        node[1] += xmlContent[i]
-                i += 1
+                        if hasAttr:
+                            if xmlContent[i] == '\'' or xmlContent[i] == '\"':
+                                inquotes = not inquotes
+                                if not inquotes and key != "" and val != "":
+                                    d[key] = val
+                                    key = "@"
+                                    val = ""
+                            elif inquotes:
+                                val += xmlContent[i]
+                            elif xmlContent[i] != '=' and not isSpace:
+                                key += xmlContent[i]
+                        else:
+                            element_name += xmlContent[i]
+                    i += 1
+                item = curr_dict.get(element_name)
+                if item is None:
+                    curr_dict[element_name] = d
+                else:
+                    if isinstance(item, list):
+                        item.append(d)
+                    else:
+                        curr_dict[element_name] = [item, d]
+                if isContainer:
+                    curr_dict = d
+                    containerStack.append(d)
             i += 1
-            nodes.append(node)
         else:
-            node[0] = NodeType.TEXT
-            isSpace = False
             j = xmlContent.find('<', i + 1)
             if j < 0:
-                return nodes
-            node[1] = xmlContent[i:j].strip()
+                return containerStack.pop()
+            element_name = xmlContent[i:j].strip()
             i = j
-            if len(node[1]) > 0:
-                nodes.append(node)
-
-    return nodes
-
-
-def parse(xmlContent: str):
-    nodes = splitNodes(xmlContent)
-    curr_dict = {}
-    containerStack = [curr_dict]
-    for node in nodes:
-        if node[0] == NodeType.TEXT:
-            curr_dict["#text"] = node[1]
-        elif node[0] == NodeType.PRIMITIVE or node[
-            0] == NodeType.CONTAINER_OPEN:
-            d = node[2]
-            item = curr_dict.get(node[1])
-            if item is None:
-                curr_dict[node[1]] = node[2]
-            else:
-                if isinstance(item, list):
-                    item.append(d)
-                else:
-                    curr_dict[node[1]] = [item, d]
-
-            if node[0] == NodeType.CONTAINER_OPEN:
-                curr_dict = d
-                containerStack.append(d)
-        elif node[0] == NodeType.CONTAINER_CLOSE:
-            containerStack.pop()
-            curr_dict = containerStack[-1]
+            if len(element_name) > 0:
+                curr_dict["#text"] = element_name
 
     return containerStack.pop()
