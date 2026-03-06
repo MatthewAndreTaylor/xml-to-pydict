66cfa3bb5a4573a8f58876676892365de2d745bd
Matthew Taylor <100451342+MatthewAndreTaylor@users.noreply.github.com>
2026-03-06 04:03:24 -0500

    fix packaging

--- xmlpydict/pyxmlhandler/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-
-setup(
-    include_package_data=True,
-    ext_modules=cythonize(Extension("pyxmlhandler", sources=["pyxmlhandler.pyx"])),
-)
